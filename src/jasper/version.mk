db37e7a789d7db50efd4be33c477ab6ee0dfd919
Jerry Greenberg <root@vm-vct03-01.local>
2019-03-10 15:29:03 -0700

    APS-883 jasper 2.0.14
    APS-883 no need for gsl package
    
    APS-883 no need for gsl module load

--- src/jasper/version.mk
+++ src/jasper/version.mk
@@ -4,19 +4,19 @@ endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME           = sdsc-jasper
-VERSION        = 1.900.1
-RELEASE        = 1
+VERSION        = 2.0.14
+RELEASE        = 0
 PKGROOT        = /opt/jasper
 
 SRC_SUBDIR     = jasper
 
 SOURCE_NAME    = jasper
-SOURCE_SUFFIX  = zip
+SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-ZIP_PKGS       = $(SOURCE_PKG)
+TAR_GZ_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No\nAutoProv:No
 RPM.PREFIX     = $(PKGROOT)

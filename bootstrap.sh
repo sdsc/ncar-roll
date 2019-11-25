da34828846b7bb70573276a31b9d5800842da385
Jerry Greenberg <jpg@sdsc.edu>
2019-11-25 13:14:32 -0800

    APS-1076 remove "module purge" from ncl_ncarg module (running module purge causes an infinite loop)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,6 @@
 yum install flex-devel
+yum install --nogpgcheck texlive-xtab.noarch
+yum install --nogpgcheck texlive-multirow.noarch
+yum install --nogpgcheck texlive-sectsty.noarch
+yum install --nogpgcheck texlive-tocloft.noarch
+
